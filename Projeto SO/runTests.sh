7d1c4f0417ea0738489717270d9b171d90e8daa4
Nuno <nfnvsc@hotmail.com>
2019-11-04 16:44:16 +0000

    Fixed Bug

--- Projeto SO/runTests.sh
+++ Projeto SO/runTests.sh
@@ -10,19 +10,25 @@
 
 for i in $(ls $1)
 do
-    echo InputFile=$i NumThreads=1 >> tempFile
-    ./tecnicofs-nosync $1/$i output.o 1 $4 >> tempFile
-done
 
-for i in $(ls $1)
-do
+    echo InputFile=$i NumThreads=1 >> tempFile
+    ./tecnicofs-nosync $1/$i output.o 1 1 >> tempFile
+    : > $2/$i-1.txt
+    grep "TecnicoFS\|InputFile" tempFile >> $2/$i-1.txt
+    : > tempFile
+    
     for x in $(seq 2 $3)
     do
         echo InputFile=$i NumThreads=$x >> tempFile 
         ./tecnicofs-mutex $1/$i output.o $x $4 >> tempFile
-
+        : > $2/$i-$x.txt
+        grep "TecnicoFS\|InputFile" tempFile >> $2/$i-$x.txt
+        #reset file
+        : > tempFile
     done
+    
+
+
 done
 
-grep "TecnicoFS\|InputFile" tempFile > $2
 rm tempFile
