87afbe8b9f51f5b9856b50cfd2fba9e79bdc871e
Nuno <nfnvsc@hotmail.com>
2019-11-05 15:09:37 +0000

    Fixed bug runTests.sh

--- Projeto SO/runTests.sh
+++ Projeto SO/runTests.sh
@@ -5,30 +5,28 @@
 #$4 - numbuckets
 #tempFile - temporary file to be filtered
 
-#reset file
-: > tempFile
-
 for i in $(ls $1)
 do
-
-    echo InputFile=$i NumThreads=1 >> tempFile
-    ./tecnicofs-nosync $1/$i output.o 1 1 >> tempFile
+    #reset files
     : > $2/$i-1.txt
-    grep "TecnicoFS\|InputFile" tempFile >> $2/$i-1.txt
     : > tempFile
+
+    echo InputFile=$i NumThreads=1
+    ./tecnicofs-nosync $1/$i $2/$i-1.txt 1 1 >> tempFile
+    grep "TecnicoFS" tempFile
     
     for x in $(seq 2 $3)
     do
-        echo InputFile=$i NumThreads=$x >> tempFile 
-        ./tecnicofs-mutex $1/$i output.o $x $4 >> tempFile
-        : > $2/$i-$x.txt
-        grep "TecnicoFS\|InputFile" tempFile >> $2/$i-$x.txt
-        #reset file
         : > tempFile
+        : > $2/$i-$x.txt
+
+        echo InputFile=$i NumThreads=$x 
+        ./tecnicofs-mutex $1/$i $2/$i-$x.txt $x $4 >> tempFile
+        grep "TecnicoFS" tempFile      
     done
     
 
 
 done
-
+#remove tempFile
 rm tempFile
