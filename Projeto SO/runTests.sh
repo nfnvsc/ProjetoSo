f11bd6493effe9a15bd005d6c8465fe2e8d67f1f
Nuno <nfnvsc@hotmail.com>
2019-11-11 14:54:14 +0000

    Added directory verification to runTests.sh

--- Projeto SO/runTests.sh
+++ Projeto SO/runTests.sh
@@ -5,10 +5,18 @@
 #$4 - numbuckets
 #TEMP - temporary variable to be filtered
 
+if [ ! -d "$1" ]; then
+
+    echo "Input directory not found."
+    exit 1
+    
+fi
+
+#Create outputdir if it doesnt exist
+mkdir -p $2
+
 for i in $(ls $1)
 do
-    : > $2/$i-1.txt
-    TEMP=''
 
     echo InputFile=$i NumThreads=1
     TEMP=$(./tecnicofs-nosync $1/$i $2/$i-1.txt 1 1)
@@ -16,12 +24,11 @@ do
     
     for x in $(seq 2 $3)
     do
-        : > $2/$i-$x.txt
-        TEMP=''
 
         echo InputFile=$i NumThreads=$x 
         TEMP=$(./tecnicofs-mutex $1/$i $2/$i-$x.txt $x $4)
         echo $TEMP | grep -o "TecnicoFS.*"      
+
     done
 
 done
