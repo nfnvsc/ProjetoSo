47f6a4536bce0bae6d216ffc5697fe3e1dd3f27e
Nuno <nfnvsc@hotmail.com>
2019-11-21 13:06:10 +0000

    Added create, delete, rename functions to client.

--- tecnicofs-client-api.c
+++ tecnicofs-client-api.c
@@ -1,5 +1,7 @@
 #include "tecnicofs-client-api.h"
 
+#define MAXLINE 128
+
 int sockfd, servlen;
 struct sockaddr_un serv_addr;
 
@@ -7,7 +9,7 @@ int tfsMount(char *adress){
 
     /* Cria socket stream */
     if ((sockfd= socket(AF_UNIX, SOCK_STREAM, 0) ) < 0)
-        err_dump("client: can't open stream socket");
+        perror("client: can't open stream socket");
 
     /* Primeiro uma limpeza preventiva */
     bzero((char *) &serv_addr, sizeof(serv_addr));
@@ -19,7 +21,7 @@ int tfsMount(char *adress){
     servlen = strlen(serv_addr.sun_path) + sizeof(serv_addr.sun_family);
 
     if(connect(sockfd, (struct sockaddr *) &serv_addr, servlen) < 0)
-        err_dump("client: can't connect to server");
+        perror("client: can't connect to server");
 
     return 0;
     /* Envia as linhas lidas do teclado para o socket */
@@ -30,4 +32,66 @@ int tfsUnmount(){
     /* Fecha o socket e termina */
     close(sockfd);
     exit(0);
+}
+
+void sendMessage(char* sendline){
+    int n;
+    char recvline[MAXLINE+1];
+
+    /* Envia string para sockfd.
+    Note-se que o \0 não é enviado */
+    n = strlen(sendline);
+
+    if (send(sockfd, sendline, MAXLINE, 0) != n)
+        perror("str_cli:write error on socket");
+
+    /* Tenta ler string de sockfd.
+    Note-se que tem de terminar a string com \0 */
+    n = read(sockfd, recvline, MAXLINE);
+
+    if (n<0) perror("str_cli:readline error");
+        recvline[n] = 0;
+
+    /* Envia a string para stdout */
+    fputs(recvline, stdout);
+
+    //return recvline;
+}
+
+int tfsCreate(char *filename, permission ownerPermissions, permission othersPermissions){
+    char sendline[MAXLINE];
+    char aux[3];
+
+    strcpy(sendline, "c ");
+    strcat(sendline, filename);
+
+    sprintf(aux, " %d", (int)ownerPermissions);
+    strcat(sendline, aux);
+
+    sprintf(aux, "%d", (int)othersPermissions);
+    strcat(sendline, aux);
+
+    sendMessage(sendline);
+    
+}
+
+int tfsDelete(char *filename){
+    char sendline[MAXLINE];
+
+    strcpy(sendline, "d ");
+    strcat(sendline, filename);
+
+    sendMessage(sendline);
+}
+
+int tfsRename(char *filenameOld, char *filenameNew){
+    int n;
+    char sendline[MAXLINE];
+
+    strcpy(sendline, "r ");
+    strcat(sendline, filenameOld);
+    strcat(sendline, " ");
+    strcat(sendline, filenameNew);
+
+    sendMessage(sendline);
 }
--- tecnicofs-client-api.h
+++ tecnicofs-client-api.h
@@ -6,6 +6,9 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <stdlib.h>
+#include <unistd.h> 
+
 
 
 int tfsCreate(char *filename, permission ownerPermissions, permission othersPermissions);
